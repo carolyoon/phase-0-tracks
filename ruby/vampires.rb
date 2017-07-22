75236d6d1923283945135af24f69b4a64f233e6b
Caroline Yoon <carolyoon731@gmail.com>
2017-07-22 15:48:10 -0700

    Starting over vampires.rb

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,18 +1,1 @@
-def vampire_test
-	puts "What is your name?"
-	your_name = gets.chomp
 
-	puts "How old were you?"
-	age = gets.chomp
-
-	puts "What year were you born?"
-	born_year = gets.chomp
-
-	puts "Our cafeteria serves garlic bread. Should we order some for you?"
-	garlic_bread = gets.chomp
-
-	puts "Would you like to enroll in the company's health insurance?"
-	insurance = gets.chomp
-end 
-
-vampire_test
