fc9e14f7cfc2887232e5a547ac785ae0e88964f7
Caroline Yoon <carolyoon731@gmail.com>
2017-07-22 15:51:36 -0700

    Finish first 'Do the Thing' on vampires.rb

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,1 +1,17 @@
+def interview
+	puts "What is your name?"
+	your_name = gets.chomp
 
+	puts "How old are you?"
+	age = gets.chomp
+
+	puts "What year were you born?"
+	born_year = gets.chomp
+
+	puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+	garlic_bread = gets.chomp
+
+	puts "Would you like to enroll in the company's health insurance?"
+	insurance = gets.chomp
+end
+interview
