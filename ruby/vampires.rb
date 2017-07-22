0456f46dcbf00fe9f4bc57c12f0c75de9a867a83
Caroline Yoon <carolyoon731@gmail.com>
2017-07-22 16:55:05 -0700

    Update code to vampires,rb

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -3,15 +3,31 @@ def interview
 	your_name = gets.chomp
 
 	puts "How old are you?"
-	age = gets.chomp
+	age = gets.chomp.to_i 
 
 	puts "What year were you born?"
-	born_year = gets.chomp
+	born_year = gets.chomp.to_i
+		if age == 2017 - born_year
+			return true
+		else 
+			return false
+		end
 
 	puts "Our company cafeteria serves garlic bread. Should we order some for you?"
 	garlic_bread = gets.chomp
+		if garlic_bread == "yes"
+			return true
+		else
+			return false
+		end
 
 	puts "Would you like to enroll in the company's health insurance?"
 	insurance = gets.chomp
-end
+		if insurance == "yes"
+			return true
+		else
+			return false
+		end
+	end
 interview
+
