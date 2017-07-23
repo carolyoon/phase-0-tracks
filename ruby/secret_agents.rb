7a75e2b0e9c5ad1f0aa9e9acc75f59cd08a8a8f3
Caroline Yoon <carolyoon731@gmail.com>
2017-07-22 17:58:15 -0700

    Update code in secrets_agents.rb

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,13 +1,41 @@
 
 
-#create a loop so variable value is incremented by 1
-#create index method for variable 
-#set first variable to a
-def one_letter_forward(string)
+#set counter variable to 0
+#create loop so while counter is less than the length of the string, it will use the next method on the string.
+#set counter variable to increment by one 
+#return string 
+
+def encrypt(string)
 	counter = 0
 	while counter < string.length
+		if string[counter] == "z"
+			string[counter] = "a"
+		else
 		string[counter] = string[counter].next
+		end
 		counter += 1
 	end
+	string
 end
-one_letter_forward("abc")
+
+#set counter variable to 0
+#create loop so while counter is less than the length of the string, it will use the index method on the string to figure out the letter before.
+#set counter variable to increment by one
+#return string
+
+def decrypt(string)
+	counter = 0
+	while counter < string.length
+		alphabet = "abcdefghijklmnopqrstuvwxyz"
+		string[counter] = alphabet[alphabet.index(string[counter]) - 1]
+		counter += 1
+	end	
+	string
+end
+
+encrypt("abc") 
+encrypt("zed") 
+decrypt("bcd") 
+decrypt("afe")
+
+print decrypt(encrypt("swordfish"))
