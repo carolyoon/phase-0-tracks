30de7590fb89393da17c295b2ee81e6ede1b1125
Caroline Yoon <carolyoon731@gmail.com>
2017-07-22 20:23:33 -0700

    Add changes to vampire.rb

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -4,6 +4,10 @@
 #make comparative expressions 
 #print results 
 
+	puts "How many employees will be processed?"
+	num_employees = gets.chomp
+
+	num_employees.to_i.times do 
 
 	puts "What is your name?"
 	your_name = gets.chomp
@@ -21,47 +25,63 @@
 	puts "Would you like to enroll in the company's health insurance?"
 	insurance = gets.chomp
 
+	allergies = []
 
-		current_year = 2017
-		if age == current_year - born_year
-			age = true
-		else 
-			age = false 
-		end
+	puts "List any allergies you have."
+	puts "Type 'done' when you are finished."
+	user_answers = ""
 
-		if garlic_bread == "yes"
-			garlic_bread = true
-		else
-			garlic_bread = false
-		end
+	until user_answers == "done"
+		user_answers = gets.chomp
+		if user_answers == "sunshine"
+			puts "Probably a vampire."
+			break
+		end 
+		allergies.push(user_answers)
+	end 
 
-		if insurance == "yes"
-			insurance = true
-		else
-			insurance = false
-		end
+	current_year = 2017
+	if age == current_year - born_year
+		age = true
+	else 
+		age = false 
+	end
 
+	if garlic_bread == "yes"
+		garlic_bread = true
+	else
+		garlic_bread = false
+	end
 
-if age && (garlic_bread || insurance) 
-	vampire = "Probably not a vampire."
+	if insurance == "yes"
+		insurance = true
+	else
+		insurance = false
+	end
+
+	if age && (garlic_bread || insurance) 
+		vampire = "Probably not a vampire."
+	end 
+
+	if !age && (!garlic_bread || !insurance)
+		vampire = "Probably a vampire."
+	end
+
+	if !age && (!garlic_bread && !insurance)
+		vampire = "Almost certainly a vampire."
+	end
+
+	if your_name == ("Drake Cula" || "Tu Fang")
+		vampire = "Definitely a vampire."
+	end
+
+	if vampire
+		print vampire
+	else
+		print "Results inconclusive."
+	end 
 end 
 
-if !age && (!garlic_bread || !insurance)
-	vampire = "Probably a vampire."
-end
-
-if !age && !garlic_bread && !insurance
-	vampire = "Almost certainly a vampire."
-end
-
-if your_name == ("Drake Cula" || "Tu Fang")
-	vampire = "Definitely a vampire."
-end
-
-if vampire
-	print vampire
-else
-	print "Results inconclusive"
-end 
+puts "Acutally, never mind! What do these questions have to do with anything? Let's all be friends." 
 
 
