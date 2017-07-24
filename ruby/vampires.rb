fff5c9f4d86323b14be6a18f3cf8006bbe1ac99e
Caroline Yoon <carolyoon731@gmail.com>
2017-07-23 22:40:51 -0700

    Update vampires.rb

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,87 +1,72 @@
-#write out interview questions
-#store user input 
-#assign boolean values on variables 
-#make comparative expressions 
-#print results 
+# Write out interview questions
+# Store answers as variables
+# Set conditionals using if/elsif statements
+# Create loop for interview set to the number of employees processed
+# Create loop for allergies 
+# 	"sunshine" and "done" stop the loop
+# Adjust the conditionals to include the allergies loop,
+# Print out ending message. 
 
-	puts "How many employees will be processed?"
-	num_employees = gets.chomp
+puts "How many employees will be processed?"
+employees_processed = gets.chomp.to_i
 
-	num_employees.to_i.times do 
+until employees_processed == 0 do 
 
-	puts "What is your name?"
-	your_name = gets.chomp
+puts "What is your name?"
+user_name = gets.chomp
 
-	puts "How old are you?"
-	age = gets.chomp.to_i 
+puts "How old are you?"
+user_age = gets.chomp
 
-	puts "What year were you born?"
-	born_year = gets.chomp.to_i
+puts "What year were you born?"
+year_born = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you? ('yes' or 'no')"
+order_garlic_bread = gets.chomp
+
+puts "Would you like to enroll in company's health insurance? ('yes' or 'no')"
+enroll_health_insurance = gets.chomp
+
+valid_input = false
+
+until valid_input 
+puts "Name any allergies, one at a time. Type 'done' when finished."
+allergies = gets.chomp
+  if allergies == "sunshine" 
+  puts "Probably a vampire."
+  valid_input = true
+    elsif allergies == "done"
+  valid_input = true
+  else
+  end
+end
 
 
-	puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-	garlic_bread = gets.chomp
-	
-	puts "Would you like to enroll in the company's health insurance?"
-	insurance = gets.chomp
 
-	allergies = []
+current_year = 2017
 
-	puts "List any allergies you have."
-	puts "Type 'done' when you are finished."
-	user_answers = ""
+if user_age == (current_year - year_born.to_i)
+  age_result = true
+else
+  age_result = false
+end
 
-	until user_answers == "done"
-		user_answers = gets.chomp
-		if user_answers == "sunshine"
-			puts "Probably a vampire."
-			break
-		end 
-		allergies.push(user_answers)
-	end 
 
-	current_year = 2017
-	if age == current_year - born_year
-		age = true
-	else 
-		age = false 
-	end
+if user_name == "Drake Cula" || user_name == "Tu Fang"
+puts "Definitely a vampire." 
+elsif (user_age == true) && (order_garlic_bread == "yes" || enroll_health_insurance == "yes")
+puts "Probably not a vampire."
+elsif (user_age == false) && (order_garlic_bread != "yes") && (enroll_health_insurance != "yes")
+puts "Almost certainly a vampire."
+elsif (user_age == false) && (order_garlic_bread != "yes" || enroll_health_insurance != "yes")
+puts "Probably a vampire."
+elsif (allergies != "sunshine")
+puts "Results inconclusive."
+else
+end
+employees_processed -= 1 
+end
 
-	if garlic_bread == "yes"
-		garlic_bread = true
-	else
-		garlic_bread = false
-	end
-
-	if insurance == "yes"
-		insurance = true
-	else
-		insurance = false
-	end
-
-	if age && (garlic_bread || insurance) 
-		vampire = "Probably not a vampire."
-	end 
-
-	if !age && (!garlic_bread || !insurance)
-		vampire = "Probably a vampire."
-	end
-
-	if !age && (!garlic_bread && !insurance)
-		vampire = "Almost certainly a vampire."
-	end
-
-	if your_name == ("Drake Cula" || "Tu Fang")
-		vampire = "Definitely a vampire."
-	end
-
-	if vampire
-		print vampire
-	else
-		print "Results inconclusive."
-	end 
-end 
-
-puts "Acutally, never mind! What do these questions have to do with anything? Let's all be friends." 
+print "Actually, never mind! What do these questions have to do with anything? Let's all be friends."  
 
 
