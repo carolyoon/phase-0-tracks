84462c33c7eebafac9de9b9d73157997b6a694ad
Caroline Yoon <carolyoon731@gmail.com>
2017-07-25 17:35:43 -0700

    Add method_detective.rb

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,47 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
-# => “InVeStIgAtIoN”
+"iNvEsTiGaTiOn".swapcase
+=> “InVeStIgAtIoN”
 
-# "zom".<???>
-# => “zoom”
 
-# "enhance".<???>
-# => "    enhance    "
+"zoom".squeeze
+=> “zoom”
 
-# "Stop! You’re under arrest!".<???>
-# => "STOP! YOU’RE UNDER ARREST!"
+"enhance".center(15)
+=> "    enhance    "
 
-# "the usual".<???>
-#=> "the usual suspects"
+"Stop! You’re under arrest!".upcase
+=> "STOP! YOU’RE UNDER ARREST!"
 
-# " suspects".<???>
-# => "the usual suspects"
+"the usual".insert(9, " suspects")
+=> "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
-# => "The case of the disappearing last lette"
+" suspects".insert(0, "the usual ")
+=> "the usual suspects"
 
-# "The mystery of the missing first letter".<???>
-# => "he mystery of the missing first letter"
+"The case of the disappearing last letter".chomp("r")
+=> "The case of the disappearing last lette"
 
-# "Elementary,    my   dear        Watson!".<???>
-# => "Elementary, my dear Watson!"
+"The case of the disappearing last letter".chop
+=> "The case of the disappearing last lette"
 
-# "z".<???>
-# => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+"The mystery of the missing first letter".reverse.chop.reverse
+=> "The mystery of the missing first letter"
 
-# "How many times does the letter 'a' appear in this string?".<???>
-# => 4
+"The mystery of the missing first letter".slice!(1, 38)
+=> "The mystery of the missing first letter"
+
+"Elementary,    my   dear        Watson!".squeeze 
+=> "Elementary, my dear Watson!"
+
+"Elementary,    my   dear        Watson!".split.join(" ")
+=> "Elementary, my dear Watson!"
+
+"z".replace("122 233")
+=> 122 233
+a = "z".ord
+a.to_s.insert(3, " 223")
+
+"How many times does the letter 'a' appear in this string?".count
+=> 4
