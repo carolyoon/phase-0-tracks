341aa4d9d2dff67c25eb797abdbad19a295a41f6
Caroline Yoon <carolyoon731@gmail.com>
2017-07-30 23:07:21 -0700

    Update alias manager

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,12 +1,36 @@
-first_name = "Caroline"
-last_name = "Yoon"
-reverse_name = last_name + " " + first_name
-# puts reverse_name 
 
-new_name = reverse_name.split("")
-# puts new_name
 
-new_name.map! { |letter| letter.next }
-# puts new_name
+#Swap the first and last name
+#Change all the vowels/consonants to the next vowel/consonant (OR just the next letter)
+#Provide a user interface that lets a user enter a name and get a fake name back. Let user repeat until they type "quit". Might hit "Enter " to continue
+#Store fake names in a data structure. When user exits the program, iterate through the data structure and print all the data that the user entered.
 
-puts new_name.join("").split(' ')
+def name_scrambler 
+  while gets.chomp != 'quit'
+    puts "Enter your first name: "
+    first_name = gets.chomp.downcase
+    puts "Enter your last name: "
+    last_name = gets.chomp.downcase
+  
+    new_first_name = next_letter(first_name)
+    new_last_name = next_letter(last_name)
+    fake_name = new_last_name.capitalize + " " + new_first_name.capitalize
+    puts "This is your new name: #{fake_name}." 
+  
+    new_names = {}
+    new_names["#{first_name} #{last_name}"] = fake_name 
+    puts new_names 
+  end
+end 
+
+def next_letter(name)
+ split_words = name.split("").map! do |letter|
+  letter.next 
+end
+ 
+ split_words.join("")
+end
+
+name_scrambler 
+
+puts new_names 
