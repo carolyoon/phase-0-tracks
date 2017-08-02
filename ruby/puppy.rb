c70134a7617c09813068fcaf45ed929d1d2a2032
Caroline Yoon <carolyoon731@gmail.com>
2017-08-02 10:05:47 -0700

    Design puppy class in comments

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+=begin
+	SPECIES-----------------
+	Puppy
+
+	CHARACTERISTICS---------
+	Color : varies
+	Potty-trained: yes
+	Leg count: 4
+	Name: varies
+	Size: varies 
+
+	BEHAVIOR----------------
+	Run
+	Bark
+	Sit
+	Fetch
+=end
+
