b0456f9a568f2b1d0c03b515a0cb446706a8668d
Caroline Yoon <carolyoon731@gmail.com>
2017-08-02 10:43:22 -0700

    Complete release 1

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,34 @@ class Puppy
     toy
   end
 
+  # def speak(num_barks)
+  # 	speak.times do |num_barks|
+  # 		puts "Woof!"
+  # 	end 
+  # end
+
+  def roll_over
+  	puts "*rolls over* "
+  end 
+
+  def dog_years(human_years)
+  	dog_age = human_years * 7
+  	puts dog_age 
+  end
+
+  def sit
+  	puts "*sits*"
+  end 
+
+  def initialize 
+  	puts "Initializing new puppy instance..."
+  end
 end
 
+jersey = Puppy.new
+
+jersey.fetch("toy")
+# jersey.speak(3)
+jersey.roll_over 
+jersey.dog_years(8)
+jersey.sit
