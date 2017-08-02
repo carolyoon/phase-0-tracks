61f1ac011517cbae5260c943410f4287d89d204a
Caroline Yoon <carolyoon731@gmail.com>
2017-08-02 12:55:17 -0700

    Create and store multiple instances for Puppy class in puppy_methods.rb

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,3 +1,4 @@
+#Release 1
 class Puppy
 
   def fetch(toy)
@@ -5,11 +6,11 @@ class Puppy
     toy
   end
 
-  # def speak(num_barks)
-  # 	speak.times do |num_barks|
-  # 		puts "Woof!"
-  # 	end 
-  # end
+  def speak(num_barks)
+  	num_barks.times do 
+  		puts "Woof!"
+  	end 
+  end
 
   def roll_over
   	puts "*rolls over* "
@@ -32,7 +33,47 @@ end
 jersey = Puppy.new
 
 jersey.fetch("toy")
-# jersey.speak(3)
+jersey.speak(3)
 jersey.roll_over 
 jersey.dog_years(8)
 jersey.sit
+
+#Release 2 
+=begin
+	
+-create puppy class, initialize it and add a couple instance methods
+-use a loop to make 50 instances of the class
+-modify loop to store in data structure
+-iterate over data to call instance methods.
+	
+=end
+
+
+class Puppy
+	
+	def initialize
+	end 
+
+	def high_five
+		puts "*holds one paw up to hand*"	
+	end
+
+	def growl
+		puts "Grrrrr!"
+	end
+end 
+
+pup_data = []
+
+50.times do |pup_name|
+	pup_name = Puppy.new
+	pup_data << pup_name 
+end 
+
+pup_data.each do |pup_name|
+	pup_name.high_five
+	pup_name.growl
+end 
+
+
+
