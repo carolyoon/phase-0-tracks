c4896aa74dc4dcd6912427138ac364bbc3fe9549
Caroline Yoon <carolyoon731@gmail.com>
2017-08-07 00:30:50 -0700

    Finish hangman.rb

--- ruby/hangman/hangman.rb
+++ ruby/hangman/hangman.rb
@@ -1,85 +1,70 @@
-#Create hangman game
-
-#Business logic
-	#create method to store a string as an array of letters
-	#create method to store guesses
-	#create method to iterate through array and compare guess to the letters
-		#if guess matches letter, print letter
-		#if guess doesn't match letter, print "_ "
-	#create method to loop through game
-		#repeat guesses do not count
-		#stop when guesses are twice length of word or word guessed correctly
-		#stop if word is guessed 
-
-
-#User interface
-	#write congratulatory message if win
-	#write taunting message if lose 
-
-#Test code 
-
-#Business logic
 
 class Hangman
-	attr_accessor :secret_word, :guess, :guesses
+    attr_reader :current_state, :guesses
+    attr_accessor :secret_word, :guess, :kill_program
 
     def initialize
-    	puts "Initializing new game"
-    	@guesses = []
-    	@kill_program = false
-    end
-    
+        @kill_program = false 
+        @guesses = []
+    end 
+
     def store_word(secret_word)
-    	@secret_word = secret_word.split(//)
-    	@secret_word
+        @secret_word = secret_word.split(//)
+        @secret_word
+        @current_state = "_" * @secret_word.length
     end
 
-    
-    def store_guesses(guess)
-        @guesses << guess
-        compare_guesses(@secret_word, guess)
-    end 
-        
-        def compare_guesses(secret_word, guess)
-        	@secret_word.each do |letter|
-        		if guess == letter
-        			print letter
-        		else 
-        			print "_ "
-        		end
-        		@guesses
-        	end
-        	if @guesses.include?(@secret_word) == true
-        		puts "Congrats you guessed correctly!"
-        		@endgame_flag = true
-            end 	
-        endgame_finale
+    def store_guess(guess)
+        if !@guesses.include?(guess)
+            @guesses << guess
         end
-        
-        def endgame_finale
-            if @guesses.length >= @secret_word.length * 2
-            	puts "FAILURE"
-            	@kill_program = true
-            elsif @endgame_flag == true
-            	puts "VICTORY"
-            	@kill_program = true
+    end 
+
+    def compare_guess(guess)
+        @secret_word.each do |letter|
+            if guess == letter
+            @current_state[@secret_word.index(letter)] = letter            
             end
         end
-    # end
+        @current_state
+
+    end 
+end 
+
+#user interface
+
+
+puts "Welcome to Hangman! Please enter your (secret) word to be guessed: "
+game = Hangman.new
+secret_word = gets.chomp 
+
+game.store_word(secret_word)
+
+while !game.kill_program 
+    puts "Please enter your guess (limited one letter per guess): "
+    guess = gets.chomp
+
+    game.store_guess(guess)
+
+    puts game.compare_guess(guess)
+    
+    if game.guesses.length >= game.secret_word.length * 2 
+        game.kill_program = true 
+        puts "No more guesses left! You suck at this!"
+    elsif game.current_state == game.secret_word.join("")
+        game.kill_program = true 
+        puts "Congratulations, you guessed correctly!"
+    end 
 
 end
 
 
-game = Hangman.new
-#Test code
-p game.store_word("code")
-# game.guesses = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
-# game.guesses = ["c", "d", "e"]
-p game.guesses
-p game.store_guesses("c")
-p game.store_guesses("o")
-p game.store_guesses("d")
-p game.store_guesses("e")
 
-p game.guesses
+#driver code 
+# p game.store_word("code")
+# p game.store_guess("d")
+# p game.compare_guess("d")
+# p game.compare_guess("c")
+# p game.current_state
+
 
