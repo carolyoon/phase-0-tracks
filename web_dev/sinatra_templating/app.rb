ca67f95978cea77fa9fdb0f16ac779f28f3f0533
Caroline Yoon <carolyoon731@gmail.com>
2017-08-27 17:57:53 -0700

    Update app.rb, create campus.erb

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -7,6 +7,11 @@ set :public_folder, File.dirname(__FILE__) + '/static'
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
 
+get '/campus' do
+    @campus = db.execute("SELECT * FROM campus")
+    erb :campus
+end
+
 # show students on the home page
 get '/' do
   @students = db.execute("SELECT * FROM students")
@@ -23,5 +28,9 @@ post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
 end
-
 # add static resources
+
+post '/campus' do
+	db.execute("INSERT INTO campus (name) VALUES (?)", [params['campus']])
+	redirect '/campus'
+end
